0fc37803b28e42f19e8410cc39d11e46d7aff5c1
Vincent Roma <roma.vincent@gmail.com>
2016-05-26 15:34:36 +0200

    Allow CORS

--- albertas/albertas/settings_default.py
+++ albertas/albertas/settings_default.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'gallery'
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,7 @@ STATIC_URL = '/static/'
 
 API_KEY = u''
 API_SECRET = u''
+
+CORS_ORIGIN_WHITELIST = (
+        'jardinsalbertas.com',
+    )
